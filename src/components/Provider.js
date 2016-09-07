b82de027c681dc2811b2af0898c7c7f8caa42825
Curran Kelleher <curran.kelleher@gmail.com>
2016-09-07 20:13:17 +0530

    Fix grammatical issue in error message

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -12,7 +12,7 @@ function warnAboutReceivingStore() {
   warning(
     '<Provider> does not support changing `store` on the fly. ' +
     'It is most likely that you see this error because you updated to ' +
-    'Redux 2.x and React Redux 2.x which no longer hot reload reducers ' +
+    'Redux 2.x and React Redux 2.x which no longer hot reloads reducers ' +
     'automatically. See https://github.com/reactjs/react-redux/releases/' +
     'tag/v2.0.0 for the migration instructions.'
   )
